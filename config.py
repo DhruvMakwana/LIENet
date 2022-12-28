d8ef4e42396e104d46944a5a9b644d4e1cdf1172
Dhruv Makwana <dmakwana503@gmail.com>
2022-12-28 15:27:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,24 @@
 import torch
 
 # paths
-TRAIN_DIR = "E:/research/low-light/lol_dataset/our485"
-VAL_DIR = "E:/research/low-light/lol_dataset/eval15"
+TRAIN_DIR = "../lol_dataset/our485"
+VAL_DIR = "../lol_dataset/eval15"
 TRAIN_LOGS_DIR = "logs/runs/train"
 VAL_LOGS_DIR = "logs/runs/val"
 RESULTS_DIR = "evaluations/"
-CHECKPOINT_DIR = "checkpoints/best.pth"
+CHECKPOINT_GEN_DIR = "checkpoints/best_gen.pth"
+CHECKPOINT_CRITIC_DIR = "checkpoints/best_disc.pth"
 
 # train hyperparameter
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-LEARNING_RATE = 0.0001
+LEARNING_RATE = 3e-4
 BATCH_SIZE = 4
 NUM_WORKERS = 2
-IMAGE_SIZE = (256, 256)
+IMAGE_SIZE = (512, 512)
 IN_CHANNELS = 3
 NUM_EPOCHS = 500
+CRITIC_ITERATIONS = 4
+LAMBDA_GP = 10
 LOAD_MODEL = False
 SAVE_MODEL = True
 
@@ -23,3 +26,4 @@ SAVE_MODEL = True
 CHANNEL_PRIOR_KERNEL = 15
 TMIN=0.1
 OMEGA=0.95
+ALPHA=0.4
